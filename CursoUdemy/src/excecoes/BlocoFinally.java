6bb8350bbf88329e7441084a6face2d3c57d6342
José Eduardo <jose.eduardo1201@outlook.com>
2021-08-09 23:52:23 -0300

    bloco finally

---
+++ CursoUdemy/src/excecoes/BlocoFinally.java
@@ -0,0 +1,26 @@
+package excecoes;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BlocoFinally {
+	public static void exe() {
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+}
