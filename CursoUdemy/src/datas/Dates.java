f143f567fac0adf40dca7e4a366f962b0a2f23d1
José Eduardo <jose.eduardo1201@outlook.com>
2021-08-12 19:41:43 -0300

    atualizando Dates para pacote correto

--- CursoUdemy/src/datas/Dates.java
+++ CursoUdemy/src/datas/Dates.java
@@ -1,12 +1,28 @@
-package entities;
+package datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class Dates {
+	
+	public static void obtendoUnidadeTempo() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(date));
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int minutes = calendar.get(Calendar.MINUTE);
+		int month = 1 + calendar.get(Calendar.MONTH);
+		
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
+	}
 	
 	public static void dateMethod() throws ParseException {
 		SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd/MM/yyyy");
