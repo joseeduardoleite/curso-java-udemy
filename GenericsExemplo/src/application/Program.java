dc7cb2966e118ff43bdbdb95d0cb743cb6db0677
José Eduardo <jose.eduardo1201@outlook.com>
2021-09-20 13:07:55 -0300

    Exercicio print service

--- GenericsExemplo/src/application/Program.java
+++ GenericsExemplo/src/application/Program.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 import services.PrintService;
 
+/*Deseja-se fazer um programa que leia uma quantidade N, e depois N números
+inteiros. Ao final, imprima esses números de forma organizada conforme
+exemplo. Em seguida, informar qual foi o primeiro valor informado.*/
 public class Program {
 	
 	public static void main(String[] args) {
