94ea7c4a8e4d8d7eb8fd56ddbc1832983a5d9a99
José Eduardo <jose.eduardo1201@outlook.com>
2021-09-22 13:32:33 -0300

    Lambda1/Anonymous class

--- Lambda1/src/application/Program.java
+++ Lambda1/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,7 +16,15 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		//list.sort(new MyComparator());
+		
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product o1, Product o2) {
+				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+			}
+		};
 		
 		for (Product p : list) {
 			System.out.println(p);
