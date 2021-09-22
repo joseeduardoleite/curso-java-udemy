c4d834ff9e476a0ed8cd015b5670ab1802f04cf9
José Eduardo <jose.eduardo1201@outlook.com>
2021-09-22 13:34:17 -0300

    Lambda1/Lambda 1

--- Lambda1/src/application/Program.java
+++ Lambda1/src/application/Program.java
@@ -16,15 +16,11 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		//list.sort(new MyComparator());
-		
-		Comparator<Product> comp = new Comparator<Product>() {
-
-			@Override
-			public int compare(Product o1, Product o2) {
-				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
-			}
+		Comparator<Product> comp = (o1, o2) -> {
+			return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
 		};
+		
+		list.sort(comp);
 		
 		for (Product p : list) {
 			System.out.println(p);
