fe6c53d88d42f328ae17b1fa8eab2799bca086d2
José Eduardo <jose.eduardo1201@outlook.com>
2021-09-29 22:30:18 -0300

    Demo Pipelines

--- Streams/src/application/Program.java
+++ Streams/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -10,7 +11,7 @@ public class Program {
 		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		Stream<Integer> st1 = list.stream();
-		System.out.println("\n" + Arrays.toString(st1.toArray()));
+		System.out.println(Arrays.toString(st1.toArray()));
 		
 		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
 		System.out.println("\n" + Arrays.toString(st2.toArray()));
@@ -21,5 +22,21 @@ public class Program {
 		// fibonacci
 		Stream<Long> st4 = Stream.iterate(new Long[] { 0L, 1L }, p -> new Long[] { p[1], p[0]+p[1] }).map(p -> p[0]);
 		System.out.println("\n" + Arrays.toString(st4.limit(10).toArray()));
+		
+		System.out.println("\n \n" + "Pipeline: ");
+		Program.pipeline();
+	}
+	
+	public static void pipeline() {
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println("\n" + Arrays.toString(st1.toArray()));
+		
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		System.out.println("\nSum = " + sum);
+		
+		List<Integer> newList = list.stream().filter(x -> x % 2 == 0).map(x -> x * 10).collect(Collectors.toList());
+		System.out.println("\n" + Arrays.toString(newList.toArray()));
 	}
 }
